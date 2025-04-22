2288e038fb0414c2be16deb2e98bcb7d009cdb8c
haroonkpk <haroonkhanlala47@gmail.com>
2025-04-22 20:48:52 +0500

    hogaya g

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import { all } from "axios";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: "dist", // Set output directory to public
-  },
+
 });
